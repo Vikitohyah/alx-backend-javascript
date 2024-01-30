99f5b575e7a010844ecf59b2b5241e4f32b338b4
Vikitohyah <victoriaedorh@gmail.com>
2024-01-30 23:56:57 +0000

    Node JS basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Edorh Victoria <https://github.com/Vikitohyah>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
